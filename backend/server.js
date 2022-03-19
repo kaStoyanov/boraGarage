0aae44f8900a3ae2772447224767848e079cb426
Kaloyan <eczenn@gmail.com>
2022-03-19 14:17:09 +0300

    da

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import path from 'path';
-import express  from 'express'
-import dotenv  from 'dotenv'
+import express from 'express'
+import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import morgan from 'morgan'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
-import  productRoutes  from './routes/productRoutes.js';
+import productRoutes from './routes/productRoutes.js';
 
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -15,28 +15,28 @@ connectDB()
 
 const app = express()
 
-if(process.env.NODE_ENV === 'developement'){
+if (process.env.NODE_ENV === 'developement') {
     app.use(morgan('dev'))
 }
 
 app.use(express.json())
 
-app.use('/api/products',productRoutes)
-app.use('/api/users',userRoutes)
-app.use('/api/orders',orderRoutes)
-app.get('/api/config/paypal',(req,res)=> res.send(process.env.PAYPAL_CLIENT_ID))
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
-const __dirname= path.resolve()
+const __dirname = path.resolve()
 
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'/frontend/build')))
-    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
-}else{
-    app.get('/', (req,res) =>{
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
         res.send('API is Runn....')
     })
-    
+
 }
 
 
@@ -45,5 +45,5 @@ app.use(notFound)
 app.use(errorHandler)
 
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`server runingin ${process.env.NODE_ENV } mode on port ${PORT}`.yellow.bold))
+const PORT = process.env.PORT || 8080
+app.listen(PORT, console.log(`server runingin ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
